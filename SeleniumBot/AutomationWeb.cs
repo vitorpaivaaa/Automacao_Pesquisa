dde08ca858a380da3886d8027c5b0292f139e1c4
vitoo <vitoo@VITOR>
2025-06-28 20:41:08 -0300

    Adicionar arquivos de projeto.

---
+++ SeleniumBot/AutomationWeb.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBot
+{
+    internal class AutomationWeb
+    {
+        public IWebDriver driver;
+
+        public AutomationWeb()
+        {
+            driver = new ChromeDriver();
+        }
+        public void TestWeb()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            driver.FindElement(By.Name("q")).SendKeys("Youtube");
+            driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).Click();
+
+        }
+    }
+}
---
+++ SeleniumBot/Program.cs
@@ -0,0 +1,7 @@
+﻿using SeleniumBot;
+using System;
+
+var web = new AutomationWeb();
+web.TestWeb(); // <--- Change this line
+Console.WriteLine("Test completed. Check console output from TestWeb() if any."); // Or some other message
+Console.ReadLine();
